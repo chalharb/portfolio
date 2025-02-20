457afa61680d25a78061ec2cc17da3fe16522a72
Logan Harber <chalharb@gmail.com>
2025-02-19 21:39:04 -0600

    updated homepage layout and stubbed out content

---
+++ components/global/footer.tsx
@@ -0,0 +1,43 @@
+import { Github, Linkedin, Mail } from "lucide-react";
+
+export function Footer() {
+  return (
+    <footer className="mb-16">
+      <h2 className="scroll-m-20 text-2xl font-semibold tracking-tight mb-2">
+        Get in touch
+      </h2>
+      <ul className="font-sm flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="/rss"
+          >
+            <Github />
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://github.com/vercel/next.js"
+          >
+            <Linkedin />
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://vercel.com/templates/next.js/portfolio-starter-kit"
+          >
+            <Mail />
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+}
